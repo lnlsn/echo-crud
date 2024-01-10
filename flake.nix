5865a447431026779eddbac513e252a74985adf7
LnLsn <to@lnlsn.xyz>
2024-01-10 07:59:17 -0300

    Format

--- flake.nix
+++ flake.nix
@@ -18,107 +18,96 @@
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-        overlays = [
-          (final: prev: {
-            go = prev.go_1_21;
-          })
-        ];
-      });
-    in
-      {
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ (final: prev: { go = prev.go_1_21; }) ];
+        });
+    in {
 
-        # Provide some binary packages for selected system types.
-        packages = forAllSystems (system:
-          let
-            pkgs = nixpkgsFor.${system};
-          in
-            {
-              default = pkgs.buildGo121Module {
-                pname = "echo-crud";
-                inherit version;
-                src = ./.;
-                vendorHash = null;
-              };
-            });
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in {
+          default = pkgs.buildGo121Module {
+            pname = "echo-crud";
+            inherit version;
+            src = ./.;
+            vendorHash = null;
+          };
+        });
 
-        nixosModules.default = { config, lib, pkgs, ... }:
-          with lib;
-          let
-            cfg = config.lnlsn.services.echo-crud;
-          in
-            {
-              options.lnlsn.services.echo-crud = {
-                enable = mkEnableOption "Enable the Echo Crud service";
+      nixosModules.default = { config, lib, pkgs, ... }:
+        with lib;
+        let cfg = config.lnlsn.services.echo-crud;
+        in {
+          options.lnlsn.services.echo-crud = {
+            enable = mkEnableOption "Enable the Echo Crud service";
 
-                logLevel = mkOption {
-                  type = with types; enum [ "DEBUG" "INFO" "ERROR" ];
-                  example = "DEBUG";
-                  default = "INFO";
-                  description = "log level for this application";
-                };
-
-                port = mkOption {
-                  type = types.port;
-                  default = 1323;
-                  description = "port to listen on";
-                };
-
-                package = mkOption {
-                  type = types.package;
-                  default = self.packages.${pkgs.system}.default;
-                  description = "package to use for this service (defaults to the one in the flake)";
-                };
-              };
-
-              config = mkIf cfg.enable {
-                systemd.services.echo-crud = {
-                  description = "Echo CRUD example";
-                  wantedBy = [ "multi-user.target" ];
-
-                  serviceConfig = {
-                    DynamicUser = "yes";
-                    # ExecStart = "${cfg.package}/bin/echo-crud --slog-level=${cfg.logLevel} --addr=:${toString cfg.port}";
-                    ExecStart = "${cfg.package}/bin/echo-crud";
-                    Restart = "on-failure";
-                    RestartSec = "5s";
-                  };
-                };
-              };
+            logLevel = mkOption {
+              type = with types; enum [ "DEBUG" "INFO" "ERROR" ];
+              example = "DEBUG";
+              default = "INFO";
+              description = "log level for this application";
             };
 
-        devShells.default = forAllSystems (system:
-          let pkgs = nixpkgsFor.${system};
-          in with pkgs;
-            mkShell {
-              buildInputs =
-                [ go_1_21 gotools go-tools gopls nixpkgs-fmt ];
-            });
-
-        checks.x86_64-linux =
-          let
-            pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          in
-            {
-              basic = pkgs.nixosTest ({
-                name = "Echo CRUD";
-                nodes.default = { config, pkgs, ... }: {
-                  imports = [ self.nixosModules.default ];
-                  lnlsn.services.echo-crud.enable = true;
-                };
-                testScript = ''
-              start_all()
-
-              default.wait_for_unit("echo-crud.service")
-              print(default.wait_until_succeeds(
-                curl -X POST \
-                  -H 'Content-Type: application/json' \
-                  -d '{"name":"Joe Smith"}' \
-                  localhost:1323/users
-              ))
-            '';
-              });
+            port = mkOption {
+              type = types.port;
+              default = 1323;
+              description = "port to listen on";
             };
+
+            package = mkOption {
+              type = types.package;
+              default = self.packages.${pkgs.system}.default;
+              description =
+                "package to use for this service (defaults to the one in the flake)";
+            };
+          };
+
+          config = mkIf cfg.enable {
+            systemd.services.echo-crud = {
+              description = "Echo CRUD example";
+              wantedBy = [ "multi-user.target" ];
+
+              serviceConfig = {
+                DynamicUser = "yes";
+                # ExecStart = "${cfg.package}/bin/echo-crud --slog-level=${cfg.logLevel} --addr=:${toString cfg.port}";
+                ExecStart = "${cfg.package}/bin/echo-crud";
+                Restart = "on-failure";
+                RestartSec = "5s";
+              };
+            };
+          };
+        };
+
+      devShells.default = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in with pkgs;
+        mkShell {
+          buildInputs = [ go_1_21 gotools go-tools gopls nixpkgs-fmt ];
+        });
+
+      checks.x86_64-linux = let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in {
+        basic = pkgs.nixosTest ({
+          name = "Echo CRUD";
+          nodes.default = { config, pkgs, ... }: {
+            imports = [ self.nixosModules.default ];
+            lnlsn.services.echo-crud.enable = true;
+          };
+          testScript = ''
+            start_all()
+
+            default.wait_for_unit("echo-crud.service")
+            print(default.wait_until_succeeds(
+              curl -X POST \
+                -H 'Content-Type: application/json' \
+                -d '{"name":"Joe Smith"}' \
+                localhost:1323/users
+            ))
+          '';
+        });
       };
+    };
 }
